9c0d7637de144a44a39b297e2c416298f53fd23d
Enze Chi <enze.chi@gmail.com>
2020-09-08 21:04:20 +1000

    Print greet message if test failed

--- sample02/greeter/tests/greeter_test.cpp
+++ sample02/greeter/tests/greeter_test.cpp
@@ -5,9 +5,9 @@
 int main(int argc, char **argv) {
 
   if (greeter::make_greeting("Tobi") != "Hello Tobi!") {
-    std::cout << greeter::make_greeting("Tobi") << std::endl;
     return 1;
   } else {
+    std::cout << greeter::make_greeting("Tobi") << std::endl;
     return 0;
   }
 }
